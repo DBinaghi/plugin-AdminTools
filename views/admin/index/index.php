8260a4a807b10e1caed4d569da066e2e6ed99b11
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-10-05 16:34:46 +0200

    Simplified database backup section

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -26,7 +26,7 @@
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation"><?php echo __('Block out from Public interface not-logged in users (and also from Admin interface some logged-in users), displaying instead an "Under Maintenance" sign.'); ?></p>
-		<a id="SUM" class="add-page button <?php echo $sumColor; ?>" href="?op=SUM-<?php echo $sumOperation; ?>"><?php echo $sumLabel; ?></a>
+		<a id="SUM" class="button <?php echo $sumColor; ?>" href="?op=SUM-<?php echo $sumOperation; ?>"><?php echo $sumLabel; ?></a>
 	</div>
 </div>
 
@@ -36,7 +36,7 @@
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation"><?php echo __('Reset all translations after language files have been changed manually.'); ?></p>
-		<a id="RC" class="add-page button green" href="?op=RC"><?php echo __('Reset Cache'); ?></a>
+		<a id="RC" class="button green" href="?op=RC"><?php echo __('Reset Cache'); ?></a>
 	</div>
 </div>
 
@@ -46,11 +46,7 @@
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation"><?php echo __('Backup the entire Omeka database into an SQL file and download it.'); ?></p>
-		<?php if (get_option('admin_tools_backup_download')): ?>
-			<a id="BD" class="add-page button green" href="<?php echo ADMIN_TOOLS_BACKUP_FILENAME; ?>?op=BD" download="<?php echo 'OmekaDB-backup_' . date('Ymd_His') . '.sql' ?>"><?php echo __('Backup Database'); ?></a>
-		<?php else: ?>
-			<a id="BD" class="add-page button green" href="?op=BD"><?php echo __('Backup Database'); ?></a>
-		<?php endif; ?>
+		<a id="BD" class="button green" href="?op=BD"><?php echo __('Backup Database'); ?></a>
 	</div>
 </div>
 
