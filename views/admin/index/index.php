05418206e0900cbf3997ba1a4c82f225eff8d587
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-10-19 05:06:29 +0200

    Fixed strings

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -25,7 +25,7 @@
 		<label for="SUM"><?php echo __('Site Under Maintenance'); ?></label>
 	</div>
 	<div class="inputs five columns omega">
-		<p class="explanation"><?php echo __('Block out from Public interface not-logged in users (and also from Admin interface some logged-in users), displaying instead an "Under Maintenance" sign.'); ?></p>
+		<p class="explanation"><?php echo __('Block out from Public interface not-logged in users') . (get_option('admin_tools_maintenance_scope_extended') ? __(' (and also, from Admin interface, some logged-in users)') : '') . __(', displaying instead an "Under Maintenance" sign.'); ?></p>
 		<a id="SUM" class="button <?php echo $sumColor; ?>" href="?op=SUM-<?php echo $sumOperation; ?>"><?php echo $sumLabel; ?></a>
 	</div>
 </div>
@@ -35,7 +35,7 @@
 		<label for="RC"><?php echo __('Languages Cache'); ?></label>
 	</div>
 	<div class="inputs five columns omega">
-		<p class="explanation"><?php echo __('Update all translations after language files have been changed manually.'); ?></p>
+		<p class="explanation"><?php echo __('Update all translations after language files have been changed manually, either in the active theme or in any active plugin.'); ?></p>
 		<a id="RC" class="button green" href="?op=RC"><?php echo __('Reset Cache'); ?></a>
 	</div>
 </div>
@@ -45,7 +45,7 @@
 		<label for="BD"><?php echo __('Database Backup'); ?></label>
 	</div>
 	<div class="inputs five columns omega">
-		<p class="explanation"><?php echo __('Backup the entire Omeka database into an SQL file.'); ?></p>
+		<p class="explanation"><?php echo __('Backup the entire Omeka database into an SQL file.') . (get_option('admin_tools_backup_sessions_ignore') ? __(' During the backup, data from Sessions table will be ignored.') : '') . (get_option('admin_tools_backup_download') ? __(' A copy of the file will be available for download') . (get_option('admin_tools_backup_compress') ? __(', compressed in GZip format.') : '.') : '.'); ?></p>
 		<a id="BD" class="button green" href="?op=BD"><?php echo __('Backup Database'); ?></a>
 	</div>
 </div>
@@ -55,10 +55,10 @@
 		<label for="TST"><?php echo __('Sessions Table'); ?></label>
 	</div>
 	<div class="inputs five columns omega">
-		<p class="explanation"><?php echo __('Trim Omeka\'s Sessions table') . (get_option('admin_tools_sessions_count') ? ' ' . __('(at the moment, the table contains %s records)', number_format($this->sessionsCount)) : '') . __('. Choose whether to delete sessions older than 1 week/month/year or all expired ones.'); ?></p>
-		<a id="TSTW" class="button green" href="?op=TSTW"><?php echo __('Trim (+1 week)'); ?></a>
-		<a id="TSTM" class="button green" href="?op=TSTM"><?php echo __('Trim (+1 month)'); ?></a>
+		<p class="explanation"><?php echo __('Trim Omeka\'s Sessions table') . (get_option('admin_tools_sessions_count') ? ' ' . __('(at the moment, the table contains <strong>%s</strong> records)', number_format($this->sessionsCount)) : '') . __(', choosing whether to delete sessions older than 1 year/month/week or all expired ones (at the moment, sessions expire after <strong>%s</strong> days).', $this->sessionMaxLifeTime); ?></p>
 		<a id="TSTY" class="button green" href="?op=TSTY"><?php echo __('Trim (+1 year)'); ?></a>
+		<a id="TSTM" class="button green" href="?op=TSTM"><?php echo __('Trim (+1 month)'); ?></a>
+		<a id="TSTW" class="button green" href="?op=TSTW"><?php echo __('Trim (+1 week)'); ?></a>
 		<a id="TSTE" class="button green" href="?op=TSTE"><?php echo __('Trim (expired)'); ?></a>
 	</div>
 </div>
