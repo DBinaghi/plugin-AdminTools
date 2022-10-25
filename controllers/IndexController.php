1b355e9d6ffc2a2a81c0cbb64d5c077c1557cff2
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2022-10-25 17:02:35 +0200

    Using Actions now

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,103 +3,111 @@
 	{
 		public function indexAction()
 		{
-			$message = '';
-			
-			if (isset($_GET["op"])) {
-				switch ($_GET["op"]) {
-					case "SUM-disable":
-						set_option('admin_tools_maintenance_active', 0);
-						$message = __('The website is online again.');
-						break;
-					case "SUM-enable":
-						set_option('admin_tools_maintenance_active', 1);
-						$message = __('The "Under Maintenance" sign is on, and access to the website has been limited.');
-						break;
-					case "RC":
-						$cache = Zend_Registry::get('Zend_Translate');
-						$cache::clearCache();
-						$message = __('The translations cache has been reset.');
-						break;
-					case "BD":
-						$db = get_db();
-						$dbConfig = $db->getAdapter()->getConfig();
-						$isCompressed = get_option('admin_tools_backup_compress');
-						$outputFile = ($isCompressed ? str_replace('.sql', '.gz', ADMIN_TOOLS_BACKUP_FILENAME) : ADMIN_TOOLS_BACKUP_FILENAME);
-						
-						$dumper = new Mysqldump\Mysqldump(
-							'mysql:host=' . $dbConfig['host'] . ';dbname=' . $dbConfig['dbname'], 
-							$dbConfig['username'], 
-							$dbConfig['password'],
-							array(
-								'compress' => ($isCompressed ? Mysqldump\Mysqldump::GZIP : Mysqldump\Mysqldump::NONE)
-							)
-						);
-						
-						if (get_option('admin_tools_backup_sessions_ignore')) {
-							$dumper->setTableLimits(array(
-								get_db()->getTableName('Session') => 0
-							));
-						}
-						
-						$dumper->start($outputFile);
-						$message = __('A %s backup copy of the Omeka database has been created.', ($isCompressed ? __('compressed') : ''));
-		
-						if (get_option('admin_tools_backup_download') && file_exists($outputFile)) {
-							header('Content-type: ' . ($isCompressed ? 'application/gzip' : 'text/plain'));
-							header('Content-Disposition: attachment; filename="OmekaDB-backup_' . date('Ymd_His') . ($isCompressed ? '.gz' : '.sql') . '"');
-							header('Content-Length: ' . filesize($outputFile));
-							$myInputStream = fopen($outputFile, 'rb');
-							$myOutputStream = fopen('php://output', 'wb');
-							stream_copy_to_stream($myInputStream, $myOutputStream);
-							fclose($myOutputStream);
-							fclose($myInputStream);
-							
-							exit;
-						}
-						break;
-					case "TSTW":
-						if ($this->trimSessionsTable('W')) {
-							$message = __('Omeka\'s Sessions table has been trimmed up to 1 week ago.');
-						}
-						break;
-					case "TSTM":
-						if ($this->trimSessionsTable('M')) {
-							$message = __('Omeka\'s Sessions table has been trimmed up to 1 month ago.');
-						}
-						break;
-					case "TSTY":
-						if ($this->trimSessionsTable('Y')) {
-							$message = __('Omeka\'s Sessions table has been trimmed up to 1 year ago.');
-						}
-						break;
-					case "TSTE":
-						if ($this->trimSessionsTable('E')) {
-							$message = __('Omeka\'s Sessions table has been trimmed up to all unexpired sessions.');
-						}
-						break;
-				}
-			}
-			
-			if ($message != '') {
-				$flash = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
-				$flash->addMessage($message, 'success');
-			}
-			
 			if (get_option('admin_tools_sessions_count')) {
 				$this->view->sessionsCount = $this->_getSessionsCount();
 			}
 			
 			$this->view->sessionMaxLifeTime = number_format($this->_getSessionMaxLifeTime() / (60 * 60 * 24), 0);
 		}
-
 		
+		public function backupAction()
+		{
+			$db = get_db();
+			$dbConfig = $db->getAdapter()->getConfig();
+			$isCompressed = get_option('admin_tools_backup_compress');
+			$outputFile = ($isCompressed ? str_replace('.sql', '.gz', ADMIN_TOOLS_BACKUP_FILENAME) : ADMIN_TOOLS_BACKUP_FILENAME);
+			
+			$dumper = new Mysqldump\Mysqldump(
+				'mysql:host=' . $dbConfig['host'] . ';dbname=' . $dbConfig['dbname'], 
+				$dbConfig['username'], 
+				$dbConfig['password'],
+				array(
+					'compress' => ($isCompressed ? Mysqldump\Mysqldump::GZIP : Mysqldump\Mysqldump::NONE)
+				)
+			);
+			
+			if (get_option('admin_tools_backup_sessions_ignore')) {
+				$dumper->setTableLimits(array(
+					get_db()->getTableName('Session') => 0
+				));
+			}
+			
+			$dumper->start($outputFile);
+
+			if (get_option('admin_tools_backup_download') && file_exists($outputFile)) {
+				header('Content-type: ' . ($isCompressed ? 'application/gzip' : 'text/plain'));
+				header('Content-Disposition: attachment; filename="OmekaDB-backup_' . date('Ymd_His') . ($isCompressed ? '.gz' : '.sql') . '"');
+				header('Content-Length: ' . filesize($outputFile));
+				$inputStream = fopen($outputFile, 'rb');
+				$outputStream = fopen('php://output', 'wb');
+				stream_copy_to_stream($inputStream, $outputStream);
+				fclose($outputStream);
+				fclose($inputStream);
+				exit;
+			}
+
+			$this->_helper->flashMessenger(__('A %s backup copy of the Omeka database has been created.', ($isCompressed ? __('compressed') : '')), 'success');
+			$this->_helper->redirector('index','index');
+		}
+		
+		public function resetCacheAction()
+		{
+			$cache = Zend_Registry::get('Zend_Translate');
+			$cache::clearCache();
+			
+			$this->_helper->flashMessenger(__('The translations cache has been reset.'), 'success');
+			$this->_helper->redirector('index','index');
+		}
+
+		public function maintenanceAction()
+		{
+			$op = $this->getRequest()->getParam('op');
+			if ($op == 'enable') {
+				set_option('admin_tools_maintenance_active', 1);
+				$this->_helper->flashMessenger(__('The "Under Maintenance" sign is on, and access to the website has been limited.'), 'success');
+			} else {
+				set_option('admin_tools_maintenance_active', 0);
+				$this->_helper->flashMessenger(__('The website is online again.'), 'success');
+			}
+			
+			$this->_helper->redirector('index','index');
+		}
+
+		public function trimSessionsAction()
+		{
+			$rng = $this->getRequest()->getParam('rng');
+			
+			if ($rng == 'expired') {
+				if ($this->_trimSessionsTable($rng)) {
+					$this->_helper->flashMessenger(__('Omeka\'s Sessions table has been trimmed up to all unexpired sessions.'), 'success');
+				}
+			} elseif ($rng != '') {
+				if ($this->_trimSessionsTable($rng)) {
+					$this->_helper->flashMessenger(__('Omeka\'s Sessions table has been trimmed up to 1 %s ago.', __($rng)), 'success');
+				}
+			}
+			
+			$this->_helper->redirector('index','index');
+		}
+		
+		public function deleteTagsAction()
+		{
+			$db = get_db();
+			$query = 'DELETE FROM ' . $db->getTableName('Tag') . ' WHERE id IN (SELECT t.id FROM ' . $db->getTableName('Tag') . ' t LEFT outer join ' . $db->getTableName('RecordsTag') . ' rt ON t.id = rt.tag_id GROUP BY name HAVING count(rt.id) = 0)';
+			$db->query($query);
+
+			$this->_helper->flashMessenger(__('All unused tags have been deleted.'), 'success');
+			
+			$this->_helper->redirector('index','index');
+		}
+			
 		private function _getSessionsCount() 
 		{
 			$db = get_db();
 			return $db->getTable('Session')->count();			
 		}
 		
-		public function _getSessionMaxLifeTime()
+		private function _getSessionMaxLifeTime()
 		{
 			$applicationFile = '../application/config/application.ini';
 			if (!file_exists($applicationFile)) {
@@ -112,23 +120,23 @@
 			$sessionParams = $sessionIni->toArray();
 			return $sessionParams['resources']['session']['gc_maxlifetime'];
 		}
-		
-		public function trimSessionsTable($period)
+
+		private function _trimSessionsTable($rng)
 		{
 			$date = new DateTime();
 			$db = get_db();
-
-			switch($period) {
-				case 'W':
+			
+			switch($rng) {
+				case 'week':
 					$query = 'DELETE FROM ' . $db->getTableName('Session') . ' WHERE modified < ' . $date->modify("-1 week")->getTimeStamp();
 					break;
-				case 'M':
+				case 'month':
 					$query = 'DELETE FROM ' . $db->getTableName('Session') . ' WHERE modified < ' . $date->modify("-1 month")->getTimeStamp();
 					break;
-				case 'Y':
+				case 'year':
 					$query = 'DELETE FROM ' . $db->getTableName('Session') . ' WHERE modified < ' . $date->modify("-1 year")->getTimeStamp();
 					break;
-				case 'E':
+				case 'expired':
 					$query = 'DELETE FROM ' . $db->getTableName('Session') . ' WHERE modified+lifetime < ' . $date->getTimeStamp();
 					break;
 				default:
