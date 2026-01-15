e06631ee03e00907cea33ed88ddbb8d8d07d72f3
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2026-01-15 03:34:43 +0100

    Add admin-plugins-browse.js for plugin activation UI

---
+++ views/admin/javascripts/admin-plugins-browse.js
@@ -0,0 +1,14 @@
+<script>
+	document.addEventListener("DOMContentLoaded", () => {
+	    let wrapper = document.querySelector('.content-wrapper');
+	    let section = wrapper.querySelector('div:first-of-type');
+      let div = document.getElementById('activate_deactivate_btns');
+      if (section && forms) {
+			  // move button to preferred location and unhide
+			  wrapper.insertBefore(div, section);
+			  div.classList.remove('hidden');
+		  } else {
+			  console.warn('Activate/Deactivate Plugins: Unable to find required elements in DOM tree.');
+		  }
+  });
+</script>
