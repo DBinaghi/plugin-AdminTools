d489a72c283556e42b68e4e151d8109689db46d6
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2026-01-15 03:52:27 +0100

    Refactor button placement logic in admin plugins script

--- views/admin/javascripts/admin-plugins-browse.js
+++ views/admin/javascripts/admin-plugins-browse.js
@@ -2,13 +2,18 @@
 	document.addEventListener("DOMContentLoaded", () => {
 	    let wrapper = document.querySelector('.content-wrapper');
 	    let section = wrapper.querySelector('div:first-of-type');
-		let div = document.getElementById('activate_deactivate_btns');
-		if (section && div) {
-			// move button to preferred location and unhide
-			wrapper.insertBefore(div, section);
-			div.classList.remove('hidden');
+        let div = document.getElementById('activate_deactivate_btns');
+        let flash = document.getElementById('flash');
+        if (section && flash) {
+			  // move button to preferred location and unhide
+			  flash.parentNode.insertBefore(div, flash.nextSibling);
+			  div.classList.remove('hidden');
+        } else if (section && div) {
+			  // move button to preferred location and unhide
+			  wrapper.insertBefore(div, section);
+			  div.classList.remove('hidden');
 		} else {
-			console.warn('Activate/Deactivate Plugins: Unable to find required elements in DOM tree.');
+			  console.warn('Activate/Deactivate Plugins: Unable to find required elements in DOM tree.');
 		}
-  });
+    });
 </script>
