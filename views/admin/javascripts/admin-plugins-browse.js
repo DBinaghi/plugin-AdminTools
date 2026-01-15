b08fda0ed41d2c3d7e588a4c2e6d1754025f3530
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2026-01-15 03:40:00 +0100

    Fix condition check for activate/deactivate buttons

--- views/admin/javascripts/admin-plugins-browse.js
+++ views/admin/javascripts/admin-plugins-browse.js
@@ -2,13 +2,13 @@
 	document.addEventListener("DOMContentLoaded", () => {
 	    let wrapper = document.querySelector('.content-wrapper');
 	    let section = wrapper.querySelector('div:first-of-type');
-      let div = document.getElementById('activate_deactivate_btns');
-      if (section && forms) {
-			  // move button to preferred location and unhide
-			  wrapper.insertBefore(div, section);
-			  div.classList.remove('hidden');
-		  } else {
-			  console.warn('Activate/Deactivate Plugins: Unable to find required elements in DOM tree.');
-		  }
+		let div = document.getElementById('activate_deactivate_btns');
+		if (section && div) {
+			// move button to preferred location and unhide
+			wrapper.insertBefore(div, section);
+			div.classList.remove('hidden');
+		} else {
+			console.warn('Activate/Deactivate Plugins: Unable to find required elements in DOM tree.');
+		}
   });
 </script>
