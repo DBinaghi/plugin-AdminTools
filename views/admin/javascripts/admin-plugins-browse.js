04389717a6eb71a9df082c4bcb7fcd0a0b6e020f
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2026-02-28 20:18:13 +0100

    Removed script tag, tidied-up code

--- views/admin/javascripts/admin-plugins-browse.js
+++ views/admin/javascripts/admin-plugins-browse.js
@@ -1,19 +1,18 @@
-<script>
-	document.addEventListener("DOMContentLoaded", () => {
-	    let wrapper = document.querySelector('.content-wrapper');
-	    let section = wrapper.querySelector('div:first-of-type');
-        let div = document.getElementById('activate_deactivate_btns');
-        let flash = document.getElementById('flash');
-        if (section && flash) {
-			  // move button to preferred location and unhide
-			  flash.parentNode.insertBefore(div, flash.nextSibling);
-			  div.classList.remove('hidden');
-        } else if (section && div) {
-			  // move button to preferred location and unhide
-			  wrapper.insertBefore(div, section);
-			  div.classList.remove('hidden');
-		} else {
-			  console.warn('Activate/Deactivate Plugins: Unable to find required elements in DOM tree.');
-		}
-    });
-</script>
+document.addEventListener("DOMContentLoaded", () => {
+	let wrapper = document.querySelector('.content-wrapper');
+	let section = wrapper.querySelector('div:first-of-type');
+	let div = document.getElementById('activate_deactivate_btns');
+	let flash = document.getElementById('flash');
+
+	if (section && flash) {
+		// move button to preferred location and unhide
+		flash.parentNode.insertBefore(div, flash.nextSibling);
+		div.classList.remove('hidden');
+	} else if (section && div) {
+		// move button to preferred location and unhide
+		wrapper.insertBefore(div, section);
+		div.classList.remove('hidden');
+	} else {
+		console.warn('Activate/Deactivate Plugins: Unable to find required elements in DOM tree.');
+	}
+});
